d63080572fb943d3230b3c8b7e633bf3beb18904
donwinn <114736340+donwinn@users.noreply.github.com>
2023-09-10 02:20:18 +0800

    Update 51lolyad.js

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -1,5 +1,5 @@
-var obj = JSON.parse($response.body);
+var obj = $response.body;
 if(obj){
   obj = obj.replace("s.NF$.length!==0","s.NF$.length==0");
-  $done({body:JSON.stringify(obj)});
+  $done({body:obj});
 }
