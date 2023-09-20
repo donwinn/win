f57766fcb1b937dc0e8ff83c90885eb37e145386
donwinn <114736340+donwinn@users.noreply.github.com>
2023-09-21 02:14:06 +0800

    Update CSDN.js

--- CSDN.js
+++ CSDN.js
@@ -1,4 +1,16 @@
-//移动端去除底部小程序打开按钮
-GM_addStyle('.feed-Sign-weixin {display:none !important}');//小程序打开按钮
+// ==UserScript==
+// @name         CSDN移动端
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  优化页面
+// @author       You
+// @match        https://*.csdn.net/*
+// @match        https://blog.csdn.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @grant        GM_addStyle
+// ==/UserScript==
+
 //解除复制限制
 document.querySelectorAll("style").forEach((s)=>{if((s.innerText||"").indexOf('#content_views pre')>-1){s.parentElement.removeChild(s)}});
+//小程序打开按钮
+GM_addStyle('.feed-Sign-weixin {display:none !important}');
