40fe4aa276eb86635099767ff055cda70e1b466f
donwinn <114736340+donwinn@users.noreply.github.com>
2023-10-26 00:36:46 +0800

    Update Wechatad.js

--- shadow/scripts/Wechatad.js
+++ shadow/scripts/Wechatad.js
@@ -1,8 +1,8 @@
 var obj = JSON.parse($response.body);
-if(obj){
+if(Object.keys(obj).length!==0){
   obj.advertisement_num = 0;
   obj.advertisement_info = [];
   obj.can_see_complaint = 0;
   delete obj.appid;
-  $done({body:JSON.stringify(obj)}); 
 }
+$done({body:JSON.stringify(obj)});
