529215b659ad06a875fc1217dafa92e0fddcb754
donwinn <114736340+donwinn@users.noreply.github.com>
2023-11-05 18:03:40 +0800

    Create shenhuH.js

---
+++ shadow/scripts/shenhuH.js
@@ -0,0 +1,8 @@
+var headers = $request.headers;
+headers['user-info'] = '00000000';
+headers['uid'] = '00000000';
+headers['system'] = '{"name":"aVBob25l","systemName":"iOS","systemVersion":"14.4","model":"iPhone","localizedModel":"iPhone","identifierForVendor":"8DBD3071-D511-4E34-A572-D73741159CE3","isPhysicalDevice":true,"isTF":"0","bundleID":"com.weplayshzb.poker1"}';
+headers['token'] = '00000000000000000000000000000000000000';
+$done({
+    'headers': headers
+});
