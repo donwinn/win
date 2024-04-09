a35d00a47314c5a74d7ec5e7ac6030fd5821c308
donwinn <114736340+donwinn@users.noreply.github.com>
2024-04-09 18:06:37 +0800

    Update dyls.js

--- shadow/scripts/dyls.js
+++ shadow/scripts/dyls.js
@@ -5,5 +5,5 @@ if(Object.keys(obj.data).length!==0){
     });
 };
 console.log('测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试');
-console.log(JSON.stringify(obj);
+console.log(JSON.stringify(obj));
 $done({body:JSON.stringify(obj)});
