6d30fc5ac33381bba0b6a9dcd144c24400097e95
donwinn <114736340+donwinn@users.noreply.github.com>
2024-04-09 17:54:27 +0800

    Create dyls.js

---
+++ shadow/scripts/dyls.js
@@ -0,0 +1,9 @@
+var obj = JSON.parse($response.body);
+if(Object.keys(obj.data).length!==0){
+    Object.keys(obj.data).forEach(key =>{
+      obj.data.key = {};
+    };
+};
+console.log('测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试');
+console.log(JSON.stringify(obj);
+$done({body:JSON.stringify(obj)});
