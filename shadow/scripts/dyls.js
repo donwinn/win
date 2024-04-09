f7a6665f47169b170b590786159101f0f34997cc
donwinn <114736340+donwinn@users.noreply.github.com>
2024-04-09 20:48:46 +0800

    Update dyls.js

--- shadow/scripts/dyls.js
+++ shadow/scripts/dyls.js
@@ -9,7 +9,7 @@ if(currentURL.includes('advert')){
     if(Object.keys(obj.data).length!==0){
         Object.keys(obj.data).forEach(key1 =>{
             if(obj.data[key1].layout == 'advert_self'){
-                obj.data[key1].data.data.image ='';
+                obj.data[key1].data.data.image ='https://raw.githubusercontent.com/donwinn/win/main/shadow/img.png';
                 obj.data[key1].data.data.url ='';
             }else if(obj.data[key1].layout == 'index_recommend_carousel'){
                 let relist = []; 
