4f45e46d391d408fc08dfa195ec81813f2172cbe
donwinn <114736340+donwinn@users.noreply.github.com>
2024-04-09 17:59:52 +0800

    Update dyls.js

--- shadow/scripts/dyls.js
+++ shadow/scripts/dyls.js
@@ -2,7 +2,7 @@ var obj = JSON.parse($response.body);
 if(Object.keys(obj.data).length!==0){
     Object.keys(obj.data).forEach(key =>{
       obj.data.key = {};
-    };
+    });
 };
 console.log('测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试');
 console.log(JSON.stringify(obj);
