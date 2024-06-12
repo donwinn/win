d1c51fce893db92d5f9bc51543362bd2ec43ce2a
donwinn <114736340+donwinn@users.noreply.github.com>
2024-06-13 05:04:44 +0800

    Update 51lolyad.js

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -2,5 +2,5 @@ var obj = $response.body;
 if(Object.keys(obj).length!==0){
   obj = obj.replace("s.NF$.length!==0","false").replace("J.bwh(r,s.c)","J.bwh(r,s.c.replace(s.c.split('.')[0].split('//')[1],'long'))");
 }
-console.lgo('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT');
+console.log('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT');
 $done({body:obj});
