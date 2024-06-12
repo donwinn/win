c85f48880a439fd0f2926a6e437f239bbf06e04f
donwinn <114736340+donwinn@users.noreply.github.com>
2024-06-13 01:36:09 +0800

    ad

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -1,5 +1,5 @@
 var obj = $response.body;
 if(Object.keys(obj).length!==0){
-  obj = obj.replace("s.NF$.length!==0","s.NF$.length==0");
+  obj = obj.replace("s.NF$.length!==0","!1");
 }
 $done({body:obj});
