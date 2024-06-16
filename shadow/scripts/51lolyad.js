43e8025519700d079e862fa411ac6c76ebe379ee
donwinn <114736340+donwinn@users.noreply.github.com>
2024-06-16 10:21:26 +0800

    Update 51lolyad.js

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -1,5 +1,5 @@
 var body = $response.body;
 var data = body.replace("s.NF$.length!==0","!1");
-body = data.replace("J.bwh(r,s.c)","J.bwh(r,s.c=s.c.replace(s.c.split('.')[0],'https://long'))");
+body = data.replace("J.bwh(r,s.c)","var m3url=s.c\\ns.c=m3url.replace('120play','long').replace('10play','long')\\nJ.bwh(r,s.c)");
 console.log('修改后数据：',body);
 $done({body:body});
