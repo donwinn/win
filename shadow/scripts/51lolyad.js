ee51480b0103f1a467ae82aae4c677017ce9660d
donwinn <114736340+donwinn@users.noreply.github.com>
2024-06-16 11:31:08 +0800

    Update 51lolyad.js

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -1,5 +1,5 @@
 var body = $response.body;
 var data = body.replace("s.NF$.length!==0","!1");
-body = data.replace("J.bwh(r,s.c)","var m3url=s.c,s.c=m3url.replace(\"120play\",\"long\").replace(\"10play\",\"long\"),J.bwh(r,s.c)");
+body = data.replace("p.e7(0,"oauth_id")","null");
 console.log('修改后数据：',body);
 $done({body:body});
