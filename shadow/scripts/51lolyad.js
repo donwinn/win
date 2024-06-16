f72b38f5d27c9f83a33231c7c7971189c77ded48
donwinn <114736340+donwinn@users.noreply.github.com>
2024-06-17 01:04:12 +0800

    Update 51lolyad.js

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -1,6 +1,8 @@
 var body = $response.body;
 var data = body.replace("s.NF$.length!==0","!1");//加载广告
 body = data.replace("p.e7(0,\"oauth_id\")","null");//随机id
-data = body.replace("r.fx=s==null?\\"\\":s","r.fx=s==null?\"\":s.replace(s.split(\"\.\")[0],\"https://long\")");//播放地址
+data = body.replace("r.fx=s==null\?\"\":s","r.fx=s==null?\"\":s.replace(s.split(\"\.\")[0],\"https://long\")");//播放地址
+body = data.replace("b.r&&b.f","b.r=!1,b.f=!1,!1");//跳过提示
+data = body.replace(/\"ads\"/g,'\"adspass\"');//页面广告
 console.log('修改后数据：',data);
 $done({body:data});
