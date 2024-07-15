340f6004c9c451a42ed15407aab0f880d502ed42
donwinn <114736340+donwinn@users.noreply.github.com>
2024-07-16 02:06:03 +0800

    Update 51lolyad.js

--- shadow/scripts/51lolyad.js
+++ shadow/scripts/51lolyad.js
@@ -6,5 +6,5 @@ body = data.replace("bhW\(\)\)\),!0\)","bhW())),!0)\ns=JSON.parse(q)\nif(s.data.
 //bady = data.replace("bhW\(\)\)\),!0\)","bhW())),!0)\ns=JSON.parse(q)\nif(s.data.aw_mv_perm==0){s.data.aw_mv_perm=1,s.data.vip_level=11,s.data.is_vip=!0,s.data.expired_at=2033977524}\nq=JSON.stringify(s)");//模拟会员解锁暗网限制
 data = body.replace("r.fx=s==null\?\"\":s","r.fx=s==null?\"\":s.replace(s.split(\"\.\")[0],\"https://long\")");//播放地址
 //body = data.replace("p.e7(0,\"oauth_id\")","null");//随机id
-console.log('修改后数据：',data);
+console.log('dondata：',data);
 $done({body:data});
