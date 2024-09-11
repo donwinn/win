7a09aa4b9b0b0c9281460488374106d4cb7ebf62
donwinn <114736340+donwinn@users.noreply.github.com>
2024-09-11 16:33:09 +0800

    Update trollstoreXCX.js

--- shadow/scripts/trollstoreXCX.js
+++ shadow/scripts/trollstoreXCX.js
@@ -5,6 +5,9 @@ if($request.url.includes('posts/')|$request.url.includes('wx-set')){
       obj.data.attributes.adRead = 'false';
    }else if(obj.data.attributes.adSwitch){
       obj.data.attributes.adSwitch = 'false';
+      obj.data.attributes.jiliAd = '';
+      obj.data.attributes.chapingAd = '';
+      obj.data.attributes.videoAd = ''
    }
    body = JSON.stringify(obj);
 };
