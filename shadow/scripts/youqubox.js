d07cf7ab4015ef0d5b7c54e2e0c6e33a1ef917f0
donwinn <114736340+donwinn@users.noreply.github.com>
2024-12-05 15:37:19 +0800

    Update youqubox.js

--- shadow/scripts/youqubox.js
+++ shadow/scripts/youqubox.js
@@ -3,10 +3,12 @@ if($request.url.includes('getBtns')){
     var obj = JSON.parse(body);
     obj.data[0].leiXing = 'lj';
     body = JSON.stringify(obj);
+    console.log(obj.data.length);
 }else if($request.url.includes('h5pwd')){
     var obj = JSON.parse(body);
     obj.status = 200;
     body = JSON.stringify(obj);
 };
+
 console.log("测试测试测试测试测试测试测试111111111111111111111111111111111111111111111111111111111",body);
 $done({body});
